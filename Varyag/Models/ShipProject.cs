eabab26a3468b2fe7e4b3d233d120b9664334763
old.pony1 <old.pony1@gmail.com>
2019-03-27 18:05:35 +0300

    ShipProject

---
+++ Varyag/Models/ShipProject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Varyag.Models
+{
+    public class ShipProject
+    {
+        public int ProjectId;
+        public string ProjectName;
+        public int ProjectLength;
+        public int ProjectWindth;
+        public int ProjectDeep;
+        public int ProjectVolume;
+        public int ProjectEnginePower;
+        public int ProjectSpeed;
+        public int ProjectSailArea;
+        public int ProjectSleepingAreas;
+        public int ProjectPassengerCap;
+        public int ProjectFuelCap;
+        public int ProjectFreshWaterCap;
+        public string ProjectDescription;
+        public byte 
+
+    }
+}
