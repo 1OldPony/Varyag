5660360ebf1f252da1602773831a3a6c485832d2
old.pony1 <old.pony1@gmail.com>
2019-03-29 17:15:14 +0300

    модели фото и проекта

---
+++ Varyag/Models/ProjectFoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Varyag.Models
+{
+    public class ProjectFoto
+    {
+        public int FotoId { get; set; }
+        public int ProjectId { get; set; }
+        public byte[] Foto { get; set; }
+    }
+}
--- Varyag/Models/ShipProject.cs
+++ Varyag/Models/ShipProject.cs
@@ -1,27 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Varyag.Models
 {
+    
     public class ShipProject
     {
-        public int ProjectId;
-        public string ProjectName;
-        public int ProjectLength;
-        public int ProjectWindth;
-        public int ProjectDeep;
-        public int ProjectVolume;
-        public int ProjectEnginePower;
-        public int ProjectSpeed;
-        public int ProjectSailArea;
-        public int ProjectSleepingAreas;
-        public int ProjectPassengerCap;
-        public int ProjectFuelCap;
-        public int ProjectFreshWaterCap;
-        public string ProjectDescription;
-        public byte 
+        public enum ShipType
+        {
+            Лодка, Катер, Ладья, Драккар, Яхта, Шлюпка, Шхуна, Струг, Бот
+        }
 
+        public int ProjectId;
+
+        [Required]
+        public string ProjectName { get; set; }
+        [Required]
+        public int ProjectLength { get; set; }
+        [Required]
+        public int ProjectWindth { get; set; }
+        [Required]
+        public int ProjectDeep { get; set; }
+        [Required]
+        public int ProjectVolume { get; set; }
+        public int? ProjectEnginePower { get; set; }
+        public int? ProjectSpeed { get; set; }
+        public int? ProjectSailArea { get; set; }
+        public int? ProjectSleepingAreas { get; set; }
+        public int ProjectPassengerCap { get; set; }
+        public int? ProjectFuelCap { get; set; }
+        public int? ProjectFreshWaterCap { get; set; }
+        [Required]
+        public ShipType ProjectType { get; set; }
+        [Required]
+        public string ProjectDescription { get; set; }
+
+        public bool CruiseShip { get; set; }
+        public bool StudyShip { get; set; }
+        public bool FishingShip { get; set; }
+        public bool HistoricalShip { get; set; }
+        public bool ReserchShip { get; set; }
+        public bool PassangerShip { get; set; }
+        
+        public virtual List<ProjectFoto> Fotos { get; set; }
     }
 }
