175e46c395af41a0337a43aee28c2f17d0e072fe
old.pony1 <old.pony1@gmail.com>
2019-05-30 12:07:10 +0300

    1

---
+++ Varyag/Migrations/20190530090110_Experimental.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Varyag.Migrations
+{
+    public partial class Experimental : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
