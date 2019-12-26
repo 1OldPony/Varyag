bd97207ebe1e5d7b74076a6ff39eebf30484e52c
old.pony1 <old.pony1@gmail.com>
2019-12-26 16:12:45 +0300

    починил edit проектов

--- Varyag/Startup.cs
+++ Varyag/Startup.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Varyag.Models;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Internal;
 
 namespace Varyag
 {
@@ -77,7 +71,7 @@ namespace Varyag
             app.UseMvc(
                 routes =>
             {
-                routes.MapRoute("default", "{controller=About}/{action=AboutUs}");
+                routes.MapRoute("default", "{controller=About}/{action=AboutUs}/{id?}");
             }
             );
         }
